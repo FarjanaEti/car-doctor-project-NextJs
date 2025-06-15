6dbf9924263eb313eed9da671985fcf8a81cb002
Farjana Eti <etimourakkhieti@gmail.com>
2025-06-15 23:15:17 +0600

    data posted on mongoDB

---
+++ src/app/api/service/route.js
@@ -0,0 +1,11 @@
+import dbConnect from "@/lib/dbConnect";
+import { NextResponse } from "next/server";
+
+export const POST = async (req) => {
+
+    const body = await req.json();
+    const bookingCollection = dbConnect("booking")
+    const result = await bookingCollection.insertOne(body)
+
+    return NextResponse.json(result)
+}
